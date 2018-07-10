d1329e47575b3f82f28bf4a41eb1543db199963d
Brandon Arnold <br@ndonarnold.email>
2018-07-09 22:02:18 -0500

    get rid of custom file indirection which isn't helping anything

--- init.el
+++ init.el
@@ -2,11 +2,33 @@
 (unless (server-running-p)
   (server-start))
 
-;; System-specific auto-generated custom files
-(cond ((eq system-type 'windows-nt) (setq custom-file "~/.emacs.d/lisp/custom-file-windows.el"))
-      ((eq system-type 'darwin) (setq custom-file "~/.emacs.d/lisp/custom-file-osx.el"))
-      (t (setq custom-file "~/.emacs.d/lisp/custom-file-linux.el")))
-(load custom-file)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("0c71e4d0b5ad79a7cb155f180adcc93f2fe5ae3d3a863de7d3a8c898087d890c" default)))
+ '(menu-bar-mode nil)
+ '(minimap-always-recenter nil)
+ '(minimap-width-fraction 0.15)
+ '(minimap-window-location (quote right))
+ '(package-selected-packages
+   (quote
+    (ace-window alchemist auctex auto-compile benchmark-init circe cmake-font-lock cmake-ide cmake-project color-theme company company-tern cpputils-cmake dired+ docker dockerfile-mode elixir-mode expand-region flycheck-credo flycheck-demjsonlint flycheck-dialyxir flycheck-dogma flycheck-elixir flycheck-mix flymd free-keys gitconfig-mode gitlab haskell-mode helm helm-core helm-css-scss helm-flycheck helm-flyspell hide-lines indium ivy iy-go-to-char js2-mode js2-refactor json-mode less-css-mode macrostep magit markdown-mode+ mmm-mode night-owl-theme ob-elixir ob-typescript omnisharp org org-projectile org-projectile-helm paredit powershell prettier-js projectile rainbow-delimiters rtags sass-mode slime smartparens sublimity tide treemacs typescript-mode use-package visual-fill-column web-mode window-purpose xref-js2 yaml-mode yasnippet yasnippet-snippets)))
+ '(scroll-bar-mode t)
+ '(tool-bar-mode nil))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "Fira Code" :foundry "outline" :slant normal :weight normal :height 110 :width normal)))))
+;; OSX font:
+;; '(default ((t (:family "Menlo" :foundry "nil" :slant normal :weight normal :height 140 :width normal))))
+;; Windows font:
+;; '(default ((t (:family "Fira Code" :foundry "outline" :slant normal :weight normal :height 110 :width normal))))
 
 ;; Emacs packages setup
 (require 'package)
