401c1b003998d7e7544d320cdae59ec93d946931
Brandon Arnold <br@ndonarnold.email>
2018-12-12 06:58:49 -0600

    feat(general): ability to move a line up and down

--- lisp/init-general.el
+++ lisp/init-general.el
@@ -60,3 +60,22 @@
 ;;  causing backslash hell
 (setq reb-re-syntax 'string)
 
+;; convenient ability to move line up and down
+;; TODO move to functions and leave keybindings
+(defun move-line-up ()
+  "Move up the current line."
+  (interactive)
+  (transpose-lines 1)
+  (forward-line -2)
+  (indent-according-to-mode))
+
+(defun move-line-down ()
+  "Move down the current line."
+  (interactive)
+  (forward-line 1)
+  (transpose-lines 1)
+  (forward-line -1)
+  (indent-according-to-mode))
+
+(global-set-key [(meta shift up)]  'move-line-up)
+(global-set-key [(meta shift down)]  'move-line-down)
