f512787049721d9735042f1eff10f13256736d66
Brandon Arnold <br@ndonarnold.email>
2020-08-23 09:27:29 -0700

    set font size

--- init.el
+++ init.el
@@ -24,10 +24,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(default ((t (:family "Hack" :foundry "nil" :slant normal :weight normal :height 170 :width normal)))))
-;;
-;; '(default ((t (:family "Hack" :foundry "nil" :slant normal :weight normal :height 130 :width normal)))))
-;;
+ '(default ((t (:family "Hack" :foundry "nil" :slant normal :weight normal :height 100 :width normal)))))
 (require 'package)
 (setq package-enable-at-startup nil)
 (setq package-archives '(("org" . "http://orgmode.org/elpa/")
